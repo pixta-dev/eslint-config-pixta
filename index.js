d4cd7e503431a9b9639bf4874a2b92de107979ed
necojackarc <necojackarc@gmail.com>
2016-12-06 20:28:03 +0900

    Disable some rules

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ module.exports = {
   "rules": {
     "comma-dangle": ["error", "never"],
     "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    "no-underscore-dangle": ["error", { "allowAfterThis": true }]
+    "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off",
+    "react/jsx-filename-extension": "off",
+    "react/no-unused-prop-types": "off"
   }
 };
